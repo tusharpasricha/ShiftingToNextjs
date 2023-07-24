c5301753607893533ca8d49d9a133e8093b7f38b
tusharpasricha <tusharpasricha21@gmail.com>
2023-07-24 20:27:23 +0530

    setting routes

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../page.module.css'
+
+const About = () => {
+  return (
+    <div className={styles.main}>About page</div>
+  )
+}
+
+
+export default About
+
